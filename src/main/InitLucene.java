edf796113118c6c8a9610a0772b459d38e449add
Kaixin Zhang <zkx0804@gmail.com>
2017-09-01 19:59:22 -0400

    Add Lucene Dependency. Add some demo code just to start.

---
+++ src/main/InitLucene.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.io.File;
+
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+public class InitLucene {
+	
+	
+	
+	Directory indexDir = FSDirectory.open(new File("index-directory"));
+	
+	
+}
