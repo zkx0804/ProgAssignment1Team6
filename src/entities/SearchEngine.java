df2e49923f128f5e46f34aee6ae650b99b8a215c
Kaixin Zhang <zkx0804@gmail.com>
2017-09-04 21:13:07 -0400

    Configure treccar tool to read paragraphs from dataset. Add classes for Lucene.

---
+++ src/entities/SearchEngine.java
@@ -0,0 +1,33 @@
+package entities;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
+public class SearchEngine {
+	private IndexSearcher searcher = null;
+	private QueryParser parser = null;
+
+	public SearchEngine() throws IOException {
+		searcher = new IndexSearcher(DirectoryReader.open(FSDirectory.open(Paths.get("index-directory"))));
+		parser = new QueryParser("content", new StandardAnalyzer());
+	}
+
+	public TopDocs performSearch(String queryString, int n) throws IOException, ParseException {
+		Query query = parser.parse(queryString);
+		return searcher.search(query, n);
+	}
+
+	public Document getDocument(int docId) throws IOException {
+		return searcher.doc(docId);
+	}
+}
---
+++ src/main/ReadDataSet.java
@@ -0,0 +1,83 @@
+package main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import co.nstant.in.cbor.CborException;
+import edu.unh.cs.treccar.Data;
+import edu.unh.cs.treccar.read_data.DeserializeData;
+import entities.Paragraph;
+
+public class ReadDataSet {
+
+	public ReadDataSet() {
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Start reading data set...");
+		System.setProperty("file.encoding", "UTF-8");
+		readingDataFiles();
+	}
+
+	public ArrayList<Paragraph> getAllParagraphFromDataSet() {
+		ArrayList<Paragraph> pList = new ArrayList<Paragraph>();
+		String dataFilePath = "./DataSet/test200/train.test200.cbor.paragraphs";
+
+		FileInputStream stream = readingDataFiles(dataFilePath);
+		try {
+			for (Data.Paragraph dataP : DeserializeData.iterableParagraphs(stream)) {
+				// System.out.println(dataP.getParaId());
+				// System.out.print(dataP.getEntitiesOnly());
+				// System.out.println(dataP.getTextOnly());
+				// System.out.println();
+				Paragraph p = new Paragraph();
+				p.setParaID(dataP.getParaId());
+				p.setParaText(dataP.getTextOnly());
+				pList.add(p);
+
+			}
+		} catch (CborException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Get " + pList.size() + " paragraphs in total.");
+		return pList;
+	}
+
+	public FileInputStream readingDataFiles(String dataFilesPath) {
+		File file = new File(dataFilesPath);
+		try {
+			FileInputStream fis = new FileInputStream(file);
+			System.out.println("Total file size to read (in bytes) : " + fis.available());
+			return fis;
+		} catch (Throwable e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	// Only for test.
+	public static void readingDataFiles() throws CborException {
+		File file = new File("./DataSet/test200/train.test200.cbor.paragraphs");
+		FileInputStream fis = null;
+
+		try {
+			fis = new FileInputStream(file);
+			System.out.println("Total file size to read (in bytes) : " + fis.available());
+
+			for (Data.Paragraph p : DeserializeData.iterableParagraphs(fis)) {
+				System.out.println(p.getParaId());
+				// System.out.print(p.getEntitiesOnly());
+				System.out.println(p.getTextOnly());
+				System.out.println();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
